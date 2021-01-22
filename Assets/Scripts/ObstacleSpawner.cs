94a0dde7ad94805749a79e47a65876f5b782b7aa
Femi Ayodeji <femidotayodeji@gmail.com>
2021-01-22 23:58:30 +0100

    Added obstacle spawner script

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject[] ObstaclePrefab;
+    public float spawnTime = 1;
+    private float timer = 0;
+
+    void Update()
+    {
+     if(timer > spawnTime)
+     {
+         int rand = Random.Range(0, ObstaclePrefab.Length);
+
+         GameObject obstacle = Instantiate(ObstaclePrefab[rand]);
+         obstacle.transform.position = transform.position + new Vector3(0, 0, 0);
+         Destroy(obstacle, 15);
+         timer = 0;
+     }
+     timer += Time.deltaTime;
+    }
+}
