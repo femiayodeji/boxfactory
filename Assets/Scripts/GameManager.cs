7726e218ff19726ce36203edf15aed9ce9d9aa4d
Femi Ayodeji <femidotayodeji@gmail.com>
2021-01-24 16:09:19 +0100

    Added game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    GameObject gameOverCanvas;
+
+    void Start()
+    {
+        Time.timeScale = 1;   
+    }
+
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
