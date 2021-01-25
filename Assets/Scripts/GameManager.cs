5df27fb043fc9d6e90738750aec6e189038317bd
Femi Ayodeji <femidotayodeji@gmail.com>
2021-01-26 00:00:05 +0100

    Renamed scene to playscene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,17 +6,24 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
+    public GameObject gameStartCanvas;
     public GameObject gameOverCanvas;
 
     void Start()
     {
-        Time.timeScale = 1;   
+        Time.timeScale = 0;   
     }
 
     public void GameOver()
     {
         gameOverCanvas.SetActive(true);
         Time.timeScale = 0;
+    }
+
+    public void Play()
+    {
+        gameStartCanvas.SetActive(false);
+        Time.timeScale = 1;   
     }
 
     public void Replay()
