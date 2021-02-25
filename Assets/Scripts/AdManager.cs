504a1e33cee3d10a7c10b5bae44861d637a21f0d
femi <femidotayodeji@gmail.com>
2021-02-25 21:47:16 +0100

    Add OnInterstitialAdsClosed action

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GoogleMobileAds.Api;
@@ -10,10 +11,12 @@ public class AdManager : MonoBehaviour
     public string adInterstialId;
     public AdPosition bannerPosition;
     public bool testDevice = false;
-    public static AdManager Instance;
 
     private BannerView _bannerView;
     private InterstitialAd _interstitial;
+
+    public static AdManager Instance;
+    public static Action OnInterstitialAdsClosed;
 
     public void Awake()
     {
@@ -33,55 +36,68 @@ public class AdManager : MonoBehaviour
         MobileAds.Initialize(appId);
         this.CreateBannerAd(CreateRequest());
         this.CreateInterstitialAd(CreateRequest());
+
+        this._interstitial.OnAdClosed += InterstitialAdClosed;
     }
 
-     private AdRequest CreateRequest()
-     {
-         AdRequest request;
+    void OnDisable()
+    {
+        this._interstitial.OnAdClosed -= InterstitialAdClosed;
+    }
 
-         if (testDevice)
-         {
-            request = new AdRequest.Builder().AddTestDevice(SystemInfo.deviceUniqueIdentifier).Build();
-         }
-        else
+    private void InterstitialAdClosed(object sender, EventArgs e)
+    {
+        if (OnInterstitialAdsClosed != null)
+        OnInterstitialAdsClosed();
+    }
+    
+    private AdRequest CreateRequest()
+    {
+        AdRequest request;
+
+        if (testDevice)
         {
-            request = new AdRequest.Builder().Build();            
+        request = new AdRequest.Builder().AddTestDevice(SystemInfo.deviceUniqueIdentifier).Build();
+        }
+    else
+    {
+        request = new AdRequest.Builder().Build();            
+    }
+
+    return request;
+    }
+
+    public void CreateInterstitialAd(AdRequest request)
+    {
+        this._interstitial = new InterstitialAd(adInterstialId);
+        this._interstitial.LoadAd(request);
+    }
+
+    public void ShowInterstitialAd()
+    {
+        if (this._interstitial.IsLoaded())
+        {
+            this._interstitial.Show();
         }
 
-        return request;
-     }
+        this._interstitial.LoadAd(CreateRequest());
+    }
 
-     public void CreateInterstitialAd(AdRequest request)
-     {
-         this._interstitial = new InterstitialAd(adInterstialId);
-         this._interstitial.LoadAd(request);
-     }
+    public void CreateBannerAd(AdRequest request)
+    {
+        this._bannerView = new BannerView(adBannerId, AdSize.SmartBanner, bannerPosition);
+        this._bannerView.LoadAd(request);
+        HideBanner();
+    }
 
-     public void ShowInterstitialAd()
-     {
-         if (this._interstitial.IsLoaded())
-         {
-             this._interstitial.Show();
-         }
+    public void HideBanner()
+    {
+        _bannerView.Hide();
+    }
 
-         this._interstitial.LoadAd(CreateRequest());
-     }
-
-     public void CreateBannerAd(AdRequest request)
-     {
-         this._bannerView = new BannerView(adBannerId, AdSize.SmartBanner, bannerPosition);
-         this._bannerView.LoadAd(request);
-         HideBanner();
-     }
-
-     public void HideBanner()
-     {
-         _bannerView.Hide();
-     }
-
-     public void ShowBanner()
-     {
-         _bannerView.Show();
-     }
+    public void ShowBanner()
+    {
+        _bannerView.Show();
+    }
 
 }
